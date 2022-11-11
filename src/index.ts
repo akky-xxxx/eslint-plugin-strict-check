cecff8efe38cc2f9593b45218782f0773e87324f
akky-xxxx <114.s.aki@gmail.com>
2022-11-11 23:31:12 +0900

    feat: define config for vanilla

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { VanillaMaster } from "./const/VanillaMaster"
 import { Master } from "./const/master"
 import { ReactMaster } from "./const/reactMaster"
 import { getConfig } from "./modules/getConfig"
@@ -6,10 +7,12 @@ import { getRules } from "./modules/getRules"
 const rules = Master.reduce(getRules, {})
 
 const react = getConfig(ReactMaster)
+const vanilla = getConfig(VanillaMaster)
 
 export = {
   configs: {
     react,
+    vanilla,
   },
   rules,
 }
