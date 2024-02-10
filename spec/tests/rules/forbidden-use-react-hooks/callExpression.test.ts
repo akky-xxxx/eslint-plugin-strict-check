0f97e9fac85a338c95458479c8a121de70e4a298
akky-xxxx <114.s.aki@gmail.com>
2024-02-10 23:25:08 +0900

    test: update tests to forbidden-use-react-hooks that added disallowPatterns

--- spec/tests/rules/forbidden-use-react-hooks/callExpression.test.ts
+++ spec/tests/rules/forbidden-use-react-hooks/callExpression.test.ts
@@ -1,4 +1,5 @@
 import { callExpression } from "../../../../src/rules/forbidden-use-react-hooks/modules/callExpression"
+import { importSpecifier } from "../../../../src/rules/forbidden-use-react-hooks/modules/importSpecifier"
 
 describe("forbidden-use-react-hooks.callExpression", () => {
   it("Not defined option", () => {
@@ -18,6 +19,25 @@ describe("forbidden-use-react-hooks.callExpression", () => {
       callExpression({
         options: [{}],
       }),
-    ).toThrow("Invalid options, please check it.")
+    ).toThrow(
+      "Define the only one property, from allowPatterns or disallowPatterns.",
+    )
+  })
+
+  it("Defined both allowPatterns, disallowPatterns", () => {
+    expect(() =>
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      importSpecifier({
+        options: [
+          {
+            allowPatterns: [],
+            disallowPatterns: [],
+          },
+        ],
+      }),
+    ).toThrow(
+      "Define the only one property, from allowPatterns or disallowPatterns.",
+    )
   })
 })
